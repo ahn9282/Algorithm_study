38589ea274c58e075fe2f7450a8485f6171f94b4
ahn9282 <155947365+ahn9282@users.noreply.github.com>
2024-01-10 13:29:12 +0900

    Time: 5 ms (79.16%), Space: 43.9 MB (29.83%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0){
+            return false;
+        }else{
+             int reverse = 0;
+             int a =x;
+             while(a !=0){
+                 reverse = reverse * 10 + a%10;
+                 a /= 10;
+             }
+             return (reverse == x);
+        }
+    }
+}
