2a391115a09543e15369dca277c22a8bb0147202
ahn9282 <155947365+ahn9282@users.noreply.github.com>
2024-04-30 23:18:12 +0900

    [Bronze I] Title: 평균, Time: 252 ms, Memory: 18672 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[] arr=new int[num];
+
+        for(int i=0 ; i < num;i++){
+            arr[i] = sc.nextInt();
+        }
+       long sum = 0;
+        long max=0;
+        for(int i=0 ; i < num;i++){
+            if(arr[i]>max) max = arr[i];
+            sum +=arr[i];
+        }
+
+        System.out.println(sum*100.0/num/max);
+
+    }
+}
