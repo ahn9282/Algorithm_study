a9d88d0fdd33843dcfbdb2954a65d43f2c791ac1
ahn9282 <155947365+ahn9282@users.noreply.github.com>
2024-07-01 13:11:07 +0900

    [level 0] Title: 코드 처리하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -1,16 +1,3 @@
+
 def solution(code):
-    answer = ''
-    mode = 0
-    for i in range(len(code)):
-        if mode == 0:
-            if code[i] != '1' and i % 2 == 0:
-                answer += code[i]
-            elif code[i] == '1':
-                mode = 1
-        else:
-            if code[i] != '1' and i % 2 == 1:
-                answer += code[i]
-            elif code[i] == '1':
-                mode = 0
-    answer='EMPTY'if answer==''else answer
-    return answer
+    return "".join(code.split("1"))[::2] or "EMPTY"
