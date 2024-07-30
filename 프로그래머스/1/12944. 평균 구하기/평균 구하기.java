d94d608eb87ab54a750d7eedb7e5ffe38b836f63
ahn9282 <155947365+ahn9282@users.noreply.github.com>
2024-07-30 12:39:03 +0900

    [level 1] Title: 평균 구하기, Time: 1.17 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -2,7 +2,7 @@ import java.util.*;
 class Solution {
     public double solution(int[] arr) {
 
-        double avg = (double)Arrays.stream(arr).sum() / arr.length;
-        return avg;
+       return (double)Arrays.stream(arr).sum() / arr.length;
+      
     }
 }
