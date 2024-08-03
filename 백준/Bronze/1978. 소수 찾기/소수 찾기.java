5611b86891eaff2005b641fae952a95c41d3249b
ahn9282 <155947365+ahn9282@users.noreply.github.com>
2024-08-03 17:55:37 +0900

    [Bronze II] Title: 소수 찾기, Time: 108 ms, Memory: 14028 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,32 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] arr = br.readLine().split(" ");
+
+        int cnt = 0;
+        br.close();
+        for (String s : arr) {
+            int a = Integer.parseInt(s);
+            if(a != 1){
+
+            for (int i = 2; i <= Math.round(Math.sqrt(a)); i++) {
+                if (a % i == 0) {
+                    cnt++;
+                    break;
+                }
+            }
+            }else{
+                cnt++;
+            }
+        }
+        System.out.println(N - cnt);
+
+    }
+}
