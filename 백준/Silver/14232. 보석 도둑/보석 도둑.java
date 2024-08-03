c8dfff1c6d28efbf0e9b2c48a9618143d0562a0a
ahn9282 <155947365+ahn9282@users.noreply.github.com>
2024-08-03 15:43:20 +0900

    [Silver I] Title: 보석 도둑, Time: 136 ms, Memory: 14396 KB -BaekjoonHub

---
+++ 백준/Silver/14232. 보석 도둑/보석 도둑.java
@@ -0,0 +1,35 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long num = Long.parseLong(br.readLine());
+        int now = 2;
+        int cnt = 0;
+        List<String> list = new ArrayList<>();
+        while(num != 1) {
+            if (now>=1000000) {
+                list.add(num+"");
+                cnt++;
+                break;
+            }
+            if (num % now == 0) {
+                num/=now;
+                cnt++;
+                list.add(now+"");
+            }
+            else {
+                now++;
+            }
+        }
+        System.out.println(cnt);
+        System.out.println(String.join(" ", list));
+
+    }
+}
