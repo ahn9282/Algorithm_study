09832272616c1dd91336cddc8d78bfef15f70ab2
ahn9282 <155947365+ahn9282@users.noreply.github.com>
2024-08-04 14:11:28 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1368 ms, Memory: 128324 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,42 +1,38 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int arr[][] = new int[N + 1][N + 1];
-        for(int i=1; i<=N;i++){
+        int N = Integer.valueOf(st.nextToken());
+        int count = Integer.valueOf(st.nextToken());
+        int[][] arr = new int[N][N];
+        for (int j = 0; j < N; j++) {
             st = new StringTokenizer(br.readLine());
-
-            for(int j=1; j <=N;j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
+            for (int i = 0; i < N; i++) {
+                arr[j][i] = Integer.parseInt(st.nextToken());
             }
         }
+        int[][] prefix = new int[N + 1][N + 1];
+        for (int i = 1; i < prefix.length; i++) {
+            for (int j = 1; j < prefix[0].length; j++) {
+                prefix[i][j] = prefix[i][j - 1] + prefix[i - 1][j] - prefix[i - 1][j - 1] + arr[i - 1][j - 1];
 
-        int D[][] = new int[N + 1][N + 1];
-        for(int i=1; i <=N;i++){
-            for(int j =1; j <= N;j++){
-
-                D[i][j]  = D[i][j-1] + D[i-1][j] - D[i-1][j-1] + arr[i][j];
             }
         }
-        for(int i=0 ; i < M ; i++){
+        for (int i = 0; i < count; i++) {
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
 
-            int result = D[x2][y2]-D[x2][y1-1]-D[x1-1][y2]+D[x1-1][y1-1];
-            System.out.println(result);
-        }
+            System.out.println(prefix[x2][y2] + prefix[x1-1][y1-1] -(prefix[x2][y1-1] + prefix[x1 - 1][y2]));
+            }
+        
+
     }
 }
