00afec8766eef7457e79279cb0185f5702c3ef9f
ahn9282 <155947365+ahn9282@users.noreply.github.com>
2024-08-05 22:17:50 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 104 ms, Memory: 14072 KB -BaekjoonHub

---
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static int[][] arr;
+    public static int answer = 1000000000;
+    public static int n;
+    public static int count ;
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+         arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+            count = 100000000;
+        recur(0, 0, 1,0);
+        System.out.println(answer);
+    }
+
+    public static void recur(int index, int sugar, int salt, int use) {
+
+        if(index == n){
+            if(use == 0) return;
+            int result = Math.abs(sugar - salt);
+            answer = Math.min(result, answer);
+            count = Math.min(count, use);
+            return;
+        }
+
+        recur(index + 1, sugar + arr[index][1], salt * arr[index][0], use+1);
+        recur(index + 1, sugar, salt, use);
+    }
+}
