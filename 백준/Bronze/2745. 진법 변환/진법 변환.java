8c164483ddbb8e6905211e5e29f3053134f4fc29
ahn9282 <155947365+ahn9282@users.noreply.github.com>
2024-08-08 01:49:20 +0900

    [Bronze II] Title: 진법 변환, Time: 124 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String str= st.nextToken();
+        int num = Integer.parseInt(st.nextToken());
+        int answer = 0;
+        int[] arr = new int[str.length()];
+        for (int i =0; i <str.length() ; i++) {
+            int now = str.charAt(str.length()  - 1 - i);
+            if(now >=65) {
+                now -= 55;
+            }else{
+                now -= 48;
+            }
+            answer += (int) Math.pow(num, i) * now;
+        }
+        System.out.println(answer);
+
+    }
+}
