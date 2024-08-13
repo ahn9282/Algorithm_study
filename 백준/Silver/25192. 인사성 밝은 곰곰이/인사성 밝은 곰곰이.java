4df7c753bc70f64554b1a6abbb72596248e03d93
ahn9282 <155947365+ahn9282@users.noreply.github.com>
2024-08-13 17:01:00 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 300 ms, Memory: 25820 KB -BaekjoonHub

---
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
@@ -0,0 +1,28 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Set<String> set = new HashSet<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int answer =0;
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            if(str.equals("ENTER"))
+            {
+                answer += set.size();
+                set.clear();
+                continue;
+            }
+            set.add(str);
+            if(i == N-1)answer += set.size();
+        }
+        System.out.println(answer);
+
+    }
+}
