f8d85d404a5065a6edd0cf46323910809515d35c
ahn9282 <155947365+ahn9282@users.noreply.github.com>
2024-08-23 21:42:01 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 85.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] number) {
+        int answer = 0;
+        for(int a = 0; a < number.length-2;a++){
+            for(int b = a+1 ; b < number.length - 1;b++){
+                for(int c = b+1 ; c < number.length;c++ ){
+                    int sum = number[a]+number[b]+number[c];
+                    if(sum == 0){
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
