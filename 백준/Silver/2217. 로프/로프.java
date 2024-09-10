7f8a45d3a858b9786ca2b5ab3cc903e816eef750
ahn9282 <155947365+ahn9282@users.noreply.github.com>
2024-09-11 01:52:07 +0900

    [Silver IV] Title: 로프, Time: 776 ms, Memory: 109712 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.java
@@ -0,0 +1,23 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            int a = sc.nextInt();
+            arr[i] = a;
+        }
+        Arrays.sort(arr);
+        int answer = 0;
+        int count = 1;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            answer = Math.max(arr[i] * count, answer);
+            count++;
+        }
+        System.out.println(answer);
+    }
+}
