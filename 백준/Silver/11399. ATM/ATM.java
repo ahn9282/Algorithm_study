f75355bb17f2a9639dfd43a2cab003513f4134cf
ahn9282 <155947365+ahn9282@users.noreply.github.com>
2024-09-10 23:20:43 +0900

    [Silver IV] Title: ATM, Time: 108 ms, Memory: 14404 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,37 +1,24 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+
+import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-   	
-    	int humans = Integer.parseInt(br.readLine());
-    	StringTokenizer st = new StringTokenizer(br.readLine());
-    	int time[] = new int[humans];
-   	
-    	for (int i = 0; i < humans; i++) {
-    		time[i] = Integer.parseInt(st.nextToken());
-		}
-   	
-    	int sum = 0;
-    	int totalSum = 0;
-  	
-    	Arrays.sort(time);
-   
-    	for (int i = 0; i < humans; i++) {
-			sum += time[i];
-			totalSum += sum;
-		}
-   	
-    	bw.write(String.valueOf(totalSum));
-  	
-    	bw.close();
-    	br.close();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        int before = arr[0];
+        int answer = arr[0];
+        for(int i = 1 ; i < N ; i++){
+            before += arr[i];
+            answer += before;
+        }
+        System.out.println(answer);
     }
 }
