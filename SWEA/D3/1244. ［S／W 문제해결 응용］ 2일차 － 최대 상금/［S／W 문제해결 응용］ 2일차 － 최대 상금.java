2d5b54244272c5e95b349d733c6af524ee7c7232
ahn9282 <155947365+ahn9282@users.noreply.github.com>
2024-10-11 21:15:24 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 450 ms, Memory: 20,240 KB -BaekjoonHub

---
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.*;
+
+class Solution
+{
+	 static int N;
+    static int max ;
+    static int[] arr;
+
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T;
+        T =Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        for (int count = 1; count <= T; count++) {
+            st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+            N = Integer.parseInt(st.nextToken());
+            arr = new int[str.length()];
+             max = 0;
+
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = str.charAt(i) - '0';
+            }
+
+            if (arr.length < N) {
+                N = arr.length;
+            }
+
+            DFS(0, 0);
+
+            System.out.println("#" + count + " " + max);
+            max = 0;
+        }
+    }
+
+    static void DFS(int start, int depth) {
+        if (depth == N) {
+            int result = 0;
+            for (int i = 0; i < arr.length; i++) {
+                result = result * 10 + arr[i];
+            }
+
+            max = Math.max(result, max);
+
+            return;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                swap(i, j);
+                DFS(i, depth + 1);
+                swap(i, j);
+            }
+        }
+    }
+
+    static void swap(int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+}
